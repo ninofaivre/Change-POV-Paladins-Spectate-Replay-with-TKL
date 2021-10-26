7cf61f0f947eae26c99a20f112bb458130dcdde7
Nino Faivre <60893869+ninofaivre@users.noreply.github.com>
2021-10-26 12:15:03 +0200

    Add files via upload

--- ReplaceInputFile.bat
+++ ReplaceInputFile.bat
@@ -8,7 +8,7 @@ Chcp 65001>Nul
 
 for /f "tokens=3,*" %%d in ('REG QUERY "HKCU\Control Panel\International\User Profile" /v Languages') do set LANG=%%d
 
-if %LANG:~0,5% == fr-FR (goto Français)
+if %LANG:~0,2% == fr (goto Français)
 goto English
 
 
@@ -26,7 +26,7 @@ goto Français
 copy /y "./DefaultInput.ini" "C:\Program Files (x86)\Steam\steamapps\common\Paladins\ChaosGame\Config\" > TmpFile
 set /p SteamResult= < TmpFile
 del TmpFile
-if "%SteamResult%" == "        1 fichier(s) copié(s)." (echo !\n!Le fichier par défaut a été remplacé avec succès dans le dossier de configuration de Paladins.) else (echo Erreur de répertoire Steam ^^!)
+if %SteamResult:~8,1% == 1 (echo !\n!Le fichier par défaut a été remplacé avec succès dans le dossier de configuration de Paladins pour Steam.) else (echo Erreur de répertoire Steam ^^!)
 goto EpicFr
 
 :EpicFr
@@ -34,7 +34,7 @@ if %launcher% == 1 (goto End)
 copy /y "./DefaultInput.ini" "C:\Program Files\Epic Games\Paladins\ChaosGame\Config\" > TmpFile
 set /p EpicResult= < TmpFile
 del TmpFile
-if "%EpicResult%" == "        1 fichier(s) copié(s)." (echo !\n!Le fichier par défaut a été remplacé avec succès dans le dossier de configuration de Paladins.) else (echo Erreur de répertoire Epic Games ^^!)
+if %EpicResult:~8,1% == 1 (echo !\n!Le fichier par défaut a été remplacé avec succès dans le dossier de configuration de Paladins pour Epic Games.) else (echo Erreur de répertoire Epic Games ^^!)
 
 goto End
 
@@ -53,7 +53,8 @@ goto English
 copy /y "./DefaultInput.ini" "C:\Program Files (x86)\Steam\steamapps\common\Paladins\ChaosGame\Config\" > TmpFile
 set /p SteamResult= < TmpFile
 del TmpFile
-if "%SteamResult%" == "        1 file(s) copied." (echo !\n!The default file has been successfully replaced by the custom one in the Paladins config folder.) else (echo Steam directory Error ^^!)
+echo %SteamResult:~8,1%
+if %SteamResult:~8,1% == 1 (echo !\n!The default file has been successfully replaced by the custom one in the Paladins config folder for Steam.) else (echo Steam directory Error ^^!)
 goto EpicEn
 
 :EpicEn
@@ -61,7 +62,7 @@ if %launcher% == 1 (goto End)
 copy /y "./DefaultInput.ini" "C:\Program Files\Epic Games\Paladins\ChaosGame\Config\" > TmpFile
 set /p EpicResult= < TmpFile
 del TmpFile
-if "%EpicResult%" == "        1 file(s) copied." (echo !\n!The default file has been successfully replaced by the custom one in the Paladins config folder.) else (echo Epic Game directory Error ^^!)
+if %EpicResult:~8,1% == 1 (echo !\n!The default file has been successfully replaced by the custom one in the Paladins config folder for Epic Games.) else (echo Epic Game directory Error ^^!)
 
 
 :End
